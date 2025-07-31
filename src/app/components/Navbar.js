516e1b1c7c85b33cec275ff7df30ff6406d39567
Muhammad Ilham Yushronni <hamyuz89@gmail.com>
2025-07-31 17:24:36 +0700

    fix error

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,48 +1,7 @@
-// import React from "react";
-// import Image from "next/image";
-
-// import logo from "../assets/rnrlogo.png";
-
-// export default function Navbar() {
-//   return (
-//     <nav className="flex items-center p-5 bg-[#252525]">
-//       <div className="flex items-center">
-//         <a href="/">
-//           <Image src={logo} alt="website logo" className="h-20 w-50 mb-1" />
-//         </a>
-//       </div>
-
-//       <div className="grid grid-cols-4 gap-4 w-full navbar">
-//         <div className="text-center ">
-//           <a href="/">
-//             <p>Home</p>
-//           </a>
-//         </div>
-//         <div className="text-center">
-//           <a href="/about">
-//             <p>About Us</p>
-//           </a>
-//         </div>
-//         <div className="text-center">
-//           <a href="/collection">
-//             <p>Top Collections Cut</p>
-//           </a>
-//         </div>
-//         <div className="text-center">
-//           <a href="/testimoni">
-//             <p>Testimoni</p>
-//           </a>
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// }
-
-
 import React from "react";
 import Image from "next/image";
 
-import logo from "../assets/rnrlogo.png";
+import logo from "../assets/rnrLogo.png";
 
 export default function Navbar() {
   return (
